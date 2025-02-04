17ab33c86ecdf97968bb0cb6c0078df56a9a35cc
Ilker <ilkerfrancisco@gmail.com>
2025-02-04 02:37:56 -0600

    fix: variable de entorno sqsl

--- infraestructure/db.py
+++ infraestructure/db.py
@@ -5,7 +5,10 @@ from dependency_injector.wiring import Provide, inject
 from sqlalchemy.ext.asyncio import AsyncSession
 from config.settings import settings
 # Define la URL de conexión (ajusta usuario, contraseña, host, puerto y base de datos)
-DATABASE_URL = f"postgresql+asyncpg://{settings.user}:{settings.password}@{settings.host}:{settings.port}/{settings.bd_name}"
+if settings.enviroment != "local":
+    DATABASE_URL = f"postgresql+asyncpg://{settings.user}:{settings.password}@{settings.host}/{settings.bd_name}"
+else:
+    DATABASE_URL = f"postgresql+asyncpg://{settings.user}:{settings.password}@{settings.host}:{settings.port}/{settings.bd_name}"
 
 
 engine = create_async_engine(DATABASE_URL, echo=False)
